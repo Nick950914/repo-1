e9ce50fd616d29bf39c0f1870f904eeb544878f5
Nick950914 <47797526+Nick950914@users.noreply.github.com>
2019-02-20 21:04:19 +0200

    Add files via upload
    add certificates in the middle section of footer

--- inc/footer.php
+++ inc/footer.php
@@ -13,9 +13,16 @@
 		        </ul>
       		</div>
 
+          <div class="certificates">
+            <!--<a href="img/c1.jpg"><img src="/img/c1.jpg" alt="Certificate"></a>-->
+            <a href="http://www.metimpex.com/admin/uploads/image/iso9001.PNG"><img src="http://www.metimpex.com/admin/uploads/image/iso9001.PNG"></a>
+                <a href="http://www.metimpex.com/admin/uploads/image/iso14001.PNG"><img src="http://www.metimpex.com/admin/uploads/image/iso14001.PNG"></a>
+                <a href="http://www.metimpex.com/admin/uploads/image/sert.PNG"><img src="http://www.metimpex.com/admin/uploads/image/sert.PNG"></a>       
+            </div>
+
       		
 
-      		<div class="col-md-2 footer-contact-info">
+      		<div class="footer-contact-info">
       			<address><strong>Metimpex</strong><br />
 <strong>гр. Пловдив</strong></address>
 
@@ -34,7 +41,7 @@
     </footer>
 <div style="clear:both; text-align: center; z-index:99999 !important; display:block !important; visibility:visible !important;">
                         <div style="position:relative; top:0; margin-right:auto;margin-left:auto; margin-top: 15px; z-index:99999; display:block !important; visibility:visible !important;">
-                        <center><a href="#" title="None" style="text-decoration: none; display:block !important; visibility:visible !important;">Powered by No One</a></center><br />
+                        <center><a href="#" title="None" style="text-decoration: none; margin-right: 150px; display:block !important; visibility:visible !important;">Powered by No One</a></center><br />
                         </div>
   </body>
 </html>
